548c42d9682ffa71f79dd66046be97678b32a65b
yannik2205 <bloseryannik@gmail.com>
2023-04-25 19:19:53 +0200

    starting blco adding

--- src/main/java/net/yannik/darquise/Darquise.java
+++ src/main/java/net/yannik/darquise/Darquise.java
@@ -8,6 +8,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.yannik.darquise.block.ModBlocks;
 import net.yannik.darquise.item.ModItems;
 import org.slf4j.Logger;
 
@@ -24,6 +25,7 @@ public class Darquise
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
         MinecraftForge.EVENT_BUS.register(this);
---
+++ src/main/java/net/yannik/darquise/block/ModBlocks.java
@@ -0,0 +1,43 @@
+package net.yannik.darquise.block;
+
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.yannik.darquise.Darquise;
+import net.yannik.darquise.item.ModCreativeModeTab;
+import net.yannik.darquise.item.ModItems;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, Darquise.MOD_ID);
+
+    public static final RegistryObject<Block> STEEL_BLOCK = registerBlock("steel",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(6f).requiresCorrectToolForDrops()), ModCreativeModeTab.Darquise);
+
+
+    //helper methods to create a block (basically rewriting a item to a block
+    private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab){
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn, tab);
+
+        return toReturn;
+    }
+
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block, CreativeModeTab tab){
+        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
+    }
+
+//register
+    public static void register (IEventBus eventBus){
+        BLOCKS.register(eventBus);
+    }
+}
