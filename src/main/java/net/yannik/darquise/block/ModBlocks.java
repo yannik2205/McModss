9f8493b2d85ad6c6a64974bdd88a2077a3daee7f
yannik2205 <bloseryannik@gmail.com>
2023-04-25 22:03:11 +0200

    creating ores of steel and block of steel

--- src/main/java/net/yannik/darquise/block/ModBlocks.java
+++ src/main/java/net/yannik/darquise/block/ModBlocks.java
@@ -1,9 +1,11 @@
 package net.yannik.darquise.block;
 
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -20,8 +22,15 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, Darquise.MOD_ID);
 
-    public static final RegistryObject<Block> STEEL_BLOCK = registerBlock("steel",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(6f).requiresCorrectToolForDrops()), ModCreativeModeTab.Darquise);
+    public static final RegistryObject<Block> STEEL_BLOCK = registerBlock("steel_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(8f).requiresCorrectToolForDrops()),
+            ModCreativeModeTab.Darquise);
+    public static final RegistryObject<Block> STEEL_ORE = registerBlock("steel_ore",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE).strength(6f).requiresCorrectToolForDrops(),
+            UniformInt.of(3,7)), ModCreativeModeTab.Darquise);
+    public static final RegistryObject<Block> DEEPSLATE_STEEL_ORE = registerBlock("deepslate_steel_ore",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE).strength(6f).requiresCorrectToolForDrops(),
+                    UniformInt.of(4,8)), ModCreativeModeTab.Darquise);
 
 
     //helper methods to create a block (basically rewriting a item to a block
